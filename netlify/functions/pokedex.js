8f4461ae4ec749655f282a189f8610b90323c529
Michael <pawtucketcoder@gmail.com>
2023-07-17 06:51:22 -0400

    db query now

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,15 +1,45 @@
-import fetch from 'node-fetch'
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b9275475305c31',
+    password: '345afe9b',
+    database: 'heroku_ac29c9b918bb53a',
+  });
+
+// app.use(cors());
+// app.use(express.json());
+
+var test;
 
 export const handler = async () => {
-    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
-  
-    const response = await fetch(POKE_API)
-    const data = await response.json()
-  
+    db.query('SELECT * FROM users WHERE username=?', ['test'],
+    function (err, results, fields) {
+        if (err) {
+            // problems
+            test = err;
+        } else {
+            test = JSON.stringify(results);
+            console.log(test);
+        }
+    });
+    
+    //test = results.;
+
+//    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+
+//    const response = await fetch(POKE_API)
+//    const data = await response.json();
+
     return {
-      statusCode: 200,
-      body: JSON.stringify({
-        data
-      })
+        statusCode: 200,
+        // body: JSON.stringify({
+        //     data
+        // })
+        body: test
+
     }
-  }
+}
